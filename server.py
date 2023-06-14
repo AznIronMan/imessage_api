022a1a9d8ac0b3be094643c16beffe0289e7728a
Geoff Clark <geoff@clarktribegames.com>
2023-06-13 17:53:10 -0700

    turned off SSL, fixed small issues, confirmed working

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-import os, sqlite3, sys
+import contextlib, logging, os, sqlite3, sys
 
 from datetime import datetime
-from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_sslify import SSLify
+from dotenv import load_dotenv
 
-from queries import create_query as get_messages
+from queries import create_query
 from utils import env_check, init_checks, ssl_check, start_sh_check
 
 app = Flask(__name__)
@@ -14,7 +14,7 @@ if not init_checks():
     sys.exit(1)
 
 if env_check():
-    load_dotenv()
+    pass
 else:
     print("Error loading .env file.")
     sys.exit(1)
@@ -26,30 +26,63 @@ start_sh_check()
 
 @app.route('/messages', methods=['GET'])
 def get_messages():
+    load_dotenv()
+    conn = None
     try:
         type = request.args.get('type', 'all')
         start = request.args.get('start', '2001-01-01 00:00')
         end = request.args.get('end', datetime.now().strftime("%Y-%m-%d %H:%M"))
 
-        db_path = os.getenv('DB_PATH')
+        # TO DO: Add header check for API key
+
+        db_path = os.getenv('CHAT_DB_PATH')
         my_number = os.getenv('MY_NUMBER')
 
-        conn = sqlite3.connect(db_path)
+        conn = sqlite3.connect(str(db_path))
         c = conn.cursor()
 
-        query = get_messages(type, my_number, start, end)
+        query = create_query(type, my_number, start, end)
         c.execute(query)
         rows = c.fetchall()
-        conn.close()
         return jsonify(rows)
     except Exception as e:
-        try:
-            conn.close()
-        except:
-            pass
         return jsonify({'error': str(e)})
+    finally:
+        if conn is not None:
+            conn.close()
+
 
 if __name__ == '__main__':
-    print(f'Starting iMessages API server on {os.getenv("listen_host")}:{os.getenv("listen_port")}')
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
+    
+    load_dotenv()
+
+    db_path = os.getenv('CHAT_DB_PATH')
+    my_number = os.getenv('MY_NUMBER')
+
+    if db_path is not None and os.path.exists(db_path):
+        pass
+    else:
+        raise SystemExit('CHAT_DB_PATH is not accessible. Please go to Settings > Privacy > Full Disk Access and add Terminal to it. Then re-run this script with sudo.')
+
+    port = int(os.getenv('LISTEN_PORT', '5000'))
+
+    if os.getenv('DEBUG') is 'False':
+        debug_mode = False
+    elif os.getenv('DEBUG') is 'True':
+        debug_mode = True
+    else:
+        debug_mode = False
+
+    os.system('clear')
+
+    print(f'Starting iMessages API server on {os.getenv("LISTEN_IP")}:{os.getenv("LISTEN_PORT")}')
+    print()
     print('Press Ctrl+C to quit.')
-    app.run(debug=True, host=os.getenv('listen_host'), port=os.getenv('listen_port'))
+
+    if debug_mode is False:
+        with open(os.devnull, 'w') as f, contextlib.redirect_stdout(f):
+            app.run(debug=False, host=os.getenv('LISTEN_IP'), port=os.getenv('LISTEN_PORT'))
+    else:
+        app.run(debug=True, host=os.getenv('LISTEN_IP'), port=os.getenv('LISTEN_PORT'))
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import os, platform, shutil, sys
+import os, platform, sys
 
 def create_start_script():
     from queries import startup_script
@@ -10,28 +10,33 @@ def create_start_script():
         sys.exit(1)
 
 def copy_env_example():
-    if not os.path.exists('.env'):
-        if os.path.exists('.env.example'):
-            try:
-                shutil.copy('.env.example', '.env')
-            except Exception as e:
-                print(f"Error copying .env.example .env file: {e}")
-                sys.exit(1)
-        else:
-            print(".env.example not found.  Please reclone the git repo.")
+    if os.path.exists('.env.example'):
+        try:
+            with open('.env.example', 'r') as f:
+                env_example_lines = f.readlines()
+            with open('.env', 'w') as f:
+                f.writelines(env_example_lines)
+        except Exception as e:
+            print(f"Error copying .env.example to .env file: {e}")
             sys.exit(1)
         print('.env file created from .env.example')
         update_env = input('Would you like to update the values in the new .env file? (y/n): ')
         if update_env.lower().startswith('y'):
             try:
+                env_dict = {}
                 with open('.env', 'r') as f:
-                    env_lines = f.readlines()
+                    for line in f.readlines():
+                        line = line.strip()
+                        if '=' not in line:  # Skip lines without '=' character
+                            continue
+                        key, value = line.split('=')
+                        env_dict[key] = value
+                for key in env_dict:
+                    new_value = input(f'{key} ({env_dict[key]}): ')
+                    if new_value:
+                        env_dict[key] = new_value
                 with open('.env', 'w') as f:
-                    for line in env_lines:
-                        key, value = line.strip().split('=')
-                        new_value = input(f'{key} ({value}): ')
-                        if new_value:
-                            value = new_value
+                    for key, value in env_dict.items():
                         f.write(f'{key}={value}\n')
             except Exception as e:
                 print(f"Error updating .env file.  Please try updating manually.")
@@ -40,6 +45,9 @@ def copy_env_example():
         else:
             print('Please edit the .env file manually before continuing.')
             sys.exit(0)
+    else:
+        print(".env.example not found.  Please reclone the git repo.")
+        sys.exit(1)
 
 def env_check():
     if not os.path.exists('.env'):
@@ -56,6 +64,19 @@ def env_check():
             print("Please create a .env file before continuing.")
             sys.exit(0)
     else:
+        if os.path.exists('.env'):
+            with open('.env', 'r') as f:
+                env_lines = f.readlines()
+                if len(env_lines) > 0 or any("=" in line for line in env_lines):
+                    return True
+                else:
+                    delete_create_new = input('.env file exists but is invalid. Do you want to delete it and create a new one? (y/n): ')
+                    if delete_create_new.lower().startswith('y'):
+                        os.remove('.env')
+                        copy_env_example()
+                        return True
+                    else:
+                        raise SystemExit('Invalid .env file exists. Please delete it and try again.')
         return True
     
 def init_checks():
@@ -64,16 +85,24 @@ def init_checks():
     if platform.system() != 'Darwin':
         sys.exit("This script can only be run on macOS (Darwin).")
     if env_check():
-        if not os.path.exists(os.getenv('DB_PATH')):
+        db_path = os.getenv('DB_PATH')
+        if db_path and not os.path.exists(db_path):
             sys.exit("CHAT_DB_PATH is not accessible. Please go to Settings > Privacy > Full Disk Access and add Terminal to it. Then re-run this script with sudo.")
     else:
         sys.exit("Error loading .env file.")
     return True
 
 def ssl_check():
+
+    return False
+
+    # TO DO: fix the below code to work with SSL
+
     if 'USE_SSL' in os.environ and os.environ['USE_SSL'].lower() == 'true':
         if 'SSL_CERT_PATH' in os.environ and 'SSL_PRIV_KEY_PATH' in os.environ:
-            if os.path.exists(os.environ['SSL_CERT_PATH']) and os.path.exists(os.environ['SSL_PRIV_KEY_PATH']):
+            ssl_cert_path = os.path.join(os.getcwd(), os.environ['SSL_CERT_PATH'])
+            ssl_priv_path = os.path.join(os.getcwd(), os.environ['SSL_PRIV_KEY_PATH'])
+            if os.path.exists(ssl_cert_path) and os.path.exists(ssl_priv_path):
                 print("SSL certificate and private key verified. Running with SSL.")
                 return True
             else:
